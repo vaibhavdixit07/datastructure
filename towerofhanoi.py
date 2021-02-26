1398bd526514e3e38b50e470da356ed7c7d79fa2
vaibhavdixit07 <vaibhavdixit1000@gmail.com>
2021-02-26 22:03:23 +0530

    Name change

--- towerofhanoi.py
+++ towerofhanoi.py
@@ -1,9 +1,9 @@
-def towerofhinoi(number, start=1, end=3):
+def towerofhanoi(number, start=1, end=3):
     if number:
-        towerofhinoi(number-1, start, 6-start-end)
+        towerofhanoi(number-1, start, 6-start-end)
         print(f"Disk {number} from peg {start} to peg {end}")
-        towerofhinoi(number-1, 6-start-end, end)
+        towerofhanoi(number-1, 6-start-end, end)
 
 
 if __name__ == '__main__':
-    towerofhinoi(3)
+    towerofhanoi(3)
